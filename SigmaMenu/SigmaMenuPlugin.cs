66938cbe59fe8b6f19eb13ecedd9e97a1f3a8b09
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-02-06 22:34:20 -0500

    Initial commit

---
+++ SigmaMenu/SigmaMenuPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Utilities;
+
+namespace SigmaMenu;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class SigmaMenuPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public ConfigEntry<string> ConfigName { get; private set; }
+
+    public override void Load()
+    {
+        ConfigName = Config.Bind("Fake", "Name", ":>");
+
+        Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    public static class ExamplePatch
+    {
+        public static void Postfix(PlayerControl __instance)
+        {
+            __instance.cosmetics.nameText.text = PluginSingleton<SigmaMenuPlugin>.Instance.ConfigName.Value;
+        }
+    }
+}
